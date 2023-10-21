9077e43b96ecdb980c8674e0d933185fedb63c95
joseegm04 <145757507+joseegm04@users.noreply.github.com>
2023-10-21 18:36:38 +0200

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,9 @@
+<?php
+
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+
+echo "<p>Nombre: $nombre</p>";
+echo "<p>Correo electrónico: $correo</p>";
+echo "<p>Teléfono de contacto: $telefono</p>";
